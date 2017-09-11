e14b0430cdc64d14661697dd0198c8539e5014f5
Allen <bboypupu@gmail.com>
2017-09-11 15:27:30 -0700

    [Modify] 646.cpp

--- 646_Maximum_Length_of_Pair_Chain.cpp
+++ 646_Maximum_Length_of_Pair_Chain.cpp
@@ -7,7 +7,6 @@ public:
     int findLongestChain(vector<vector<int>>& pairs) {
         int count = 0;
         sort(pairs.begin(), pairs.end(), cmp);
-        cout << pairs << endl;
         vector<int>& pair = pairs[0];
         for (int i = 0; i < pairs.size(); ++i) {
             if (i == 0 || pair[1] < pairs[i][0]) {
@@ -18,14 +17,7 @@ public:
         return count;
     }
     static bool cmp(vector<int>& a, vector<int>& b) {
-        return a[1] < b[1] && a[0] < b[0];
+        return a[1] < b[1];
     }
 };
 
-int main() {
-    array = {[[-6,9],[1,6],[8,10],[-1,4],[-6,-2],[-9,8],[-5,3],[0,3]]};
-    vector<vector<int>>& newVector = array;
-    Solution mySolution;
-    cout << mySolution.findLongestChain(newVector);
-    return 0;
-}

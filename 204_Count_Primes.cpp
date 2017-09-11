b44f7e3eb53aa73c7db3f4a1c93a9b9cd30e1f87
Allen <bboypupu@gmail.com>
2017-09-11 12:10:27 -0700

    [Modify] 204.cpp

--- 204_Count_Primes.cpp
+++ 204_Count_Primes.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int countPrimes(int n) {
         int count = 0;
-        if (n > 2) {
-            count = 1;
-        }
-        else {
+        if (n > 2) 
+            count = 1;        
+        else
             return 0;
-        }
-        int i = 3;        
+        int i = 3;                
+        vector<int> usedPrimes;
+        usedPrimes.push_back(2);
         while (i < n) {
-            if (isPrime(i)) {
+            if (isPrime(i, usedPrimes))
                 count ++;
-            }
             i += 2;
         }
         return count;
     }
-    bool isPrime(int d) {
+    bool isPrime(int d, vector<int>& primes) {
         int squarRoot = sqrt(d);
-        for (int i = 2; i <= squarRoot; i++) {
-            if (d % i == 0) {
+        for (vector<int>::iterator it = primes.begin(); *it <= squarRoot && it != primes.end(); ++it) {
+            if (d % *it == 0)
                 return false;
-            }
         }
+        primes.push_back(d);
         return true;
     }
 };
@@ -35,6 +35,7 @@ public:
 int main() {
     Solution mySolution;
     int input = 0;
+    cout << "INPUT: ";
     cin >> input;
     cout << "RESULT = " << mySolution.countPrimes(input) << endl;
     return 0;

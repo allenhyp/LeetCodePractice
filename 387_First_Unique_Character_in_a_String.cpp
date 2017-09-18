b913ded13d01164919c98f857186dd04784f219e
Allen <bboypupu@gmail.com>
2017-09-17 18:26:40 -0700

    [Add] 387.cpp (2 solutions)

---
+++ 387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        // For large string: traverse string once and the map onece.
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for (int i = 0; i < idx; i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i; 
+        }
+        for (auto &p: m) {
+            if (p.second.first == 1) idx = min(idx, p.second.second);
+        }
+        return idx == s.size() ? -1 : idx;
+    }
+
+    int firstUniqChar(string s) {
+        // For small string: traverse string twice.
+        unordered_map<char, int> m;
+        for (auto &c: s) {
+            m[c]++;
+        }
+        for (int i=0; i < s.size(); i++) {
+            if (m[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};

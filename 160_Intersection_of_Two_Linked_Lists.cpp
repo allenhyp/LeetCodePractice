88ae81c21981519ebfc0c15280de0cd00e2a0cdc
Allen <bboypupu@gmail.com>
2017-09-28 01:08:07 -0700

    [Modify] 160.cpp (LCM)

--- 160_Intersection_of_Two_Linked_Lists.cpp
+++ 160_Intersection_of_Two_Linked_Lists.cpp
@@ -45,3 +45,17 @@ public:
         return NULL;
     }
 };
+
+// LCM
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA;
+        ListNode *b = headB;
+        while (a != b) {
+            a = a? a -> next : headB;
+            b = b? b -> next : headA;
+        }
+        return a;
+    }
+};

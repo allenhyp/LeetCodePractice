57a09855f1adaba2165958490f9bb7f483f52e31
Allen <bboypupu@gmail.com>
2017-09-27 17:33:15 -0700

    [Add] 96.cpp (Greedy)

---
+++ 96_Unique_Binary_Search_Trees.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if (n <= 1) return 1;
+        unsigned int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += numTrees(i) * numTrees(n - i - 1);
+        }
+        return result;
+    }
+};

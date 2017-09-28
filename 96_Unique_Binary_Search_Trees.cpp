65e328acca28cd284864538d4c956b9a405be936
Allen <bboypupu@gmail.com>
2017-09-27 17:43:17 -0700

    [Modify] 96.cpp (Dynamic Programming)(Catalan Number)

--- 96_Unique_Binary_Search_Trees.cpp
+++ 96_Unique_Binary_Search_Trees.cpp
@@ -1,3 +1,4 @@
+// Greedy
 class Solution {
 public:
     int numTrees(int n) {
@@ -7,5 +8,21 @@ public:
             result += numTrees(i) * numTrees(n - i - 1);
         }
         return result;
+    }
+};
+
+// Dynamic Programming
+class Solution {
+public:
+    int numTrees(int n) {
+        unsigned long catalan[n + 1];
+        catalan[0] = 1, catalan[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++) {
+                catalan[i] += catalan[j] * catalan[i-j-1];
+            }
+        }
+        return catalan[n];
     }
 };

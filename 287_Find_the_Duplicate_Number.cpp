e8775db377bfec1abfcaceaf19f77b5a71270073
Allen <bboypupu@gmail.com>
2017-10-17 18:22:59 -0700

    [Modify] 287.cpp (Solution of finding a cycle in two linked list)

--- 287_Find_the_Duplicate_Number.cpp
+++ 287_Find_the_Duplicate_Number.cpp
@@ -8,3 +8,22 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if (nums.size() < 2) return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int target = 0;
+        while (target != slow) {
+            slow = nums[slow];
+            target = nums[target];
+        }
+        return target;
+    }
+};

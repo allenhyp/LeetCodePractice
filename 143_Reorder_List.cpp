f8d4b16777e91ed4952d137f3a692b2449f9cfd9
Allen <bboypupu@gmail.com>
2017-11-10 16:49:44 -0800

    [Add] 143.cpp

---
+++ 143_Reorder_List.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == NULL || head->next == NULL || head->next->next == NULL) return;
+        ListNode *p1 = head;
+        ListNode *p2 = head;
+        while (p2 && p2->next) {
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+        ListNode *head2 = p1->next;
+        p1->next = NULL;
+        p2 = head2->next;
+        head2->next = NULL;
+        while (p2) {
+            p1 = p2->next;
+            p2->next = head2;
+            head2 = p2;
+            p2 = p1;
+        }
+        p1 = head;
+        p2 = head2;
+        while (p2) {
+            auto temp = p1->next;
+            p1 = p1->next = p2;
+            p2 = temp;
+        }
+    }
+};

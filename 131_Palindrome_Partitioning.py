6dc61c3dffa6622b2cacc82d962f7c16dedd4f2a
Allen <bboypupu@gmail.com>
2017-11-20 19:04:04 -0800

    [Modify] 131.py (Real DP)

--- 131_Palindrome_Partitioning.py
+++ 131_Palindrome_Partitioning.py
@@ -6,14 +6,13 @@ class Solution:
         """
         n = len(s)
         result = {0: [[]], 1: [[s[0]]]}
+        dp = [[False for _1_ in range(n)] for ___ in range(n)]
         for i in range(1, n):
             result[i + 1] = []
             for j in range(0, i + 1):
-                if self.isPal(s[j : i + 1]):
+                if s[i] == s[j] and ((i - j <= 1) or dp[j + 1][i - 1]):
+                    dp[j][i] = True
+                    ss = s[j : i + 1]
                     for prev in result[j]:
-                        result[i + 1].append(prev + [s[j : i + 1]])
+                        result[i + 1].append(prev + [ss])
         return result[n]
-
-
-    def isPal(self, string):
-        return string == string[::-1]

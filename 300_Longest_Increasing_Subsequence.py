cf3ff8de3260298d0e59a3ce8ba7b0a374ed055e
Allen <bboypupu@gmail.com>
2017-11-28 17:26:27 -0800

    [Modify] 300.py (Binary search)

--- 300_Longest_Increasing_Subsequence.py
+++ 300_Longest_Increasing_Subsequence.py
@@ -13,3 +13,23 @@ class Solution:
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[j] + 1, dp[i])
         return max(dp)
+
+
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = int((i + j) / 2)
+                if  tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size

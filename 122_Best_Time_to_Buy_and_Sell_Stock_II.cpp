fe2ebbf3fc861f5b6f0cb12c47a8065eb518cd3c
Allen <bboypupu@gmail.com>
2018-03-10 03:33:44 -0800

    [Add] 122.cpp

---
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        if (size < 1) return 0;
+        int profit = 0;
+        for (int i = 1; i < size; i++) {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+};

e722015dbe14f219c7568a97d3710f3939d861ca
Allen Hsu <bboypupu@gmail.com>
2018-10-13 16:40:48 -0700

    [Modify] 215.cpp (priority_queue)

--- 215_Kth_Largest_Element_in_an_Array.cpp
+++ 215_Kth_Largest_Element_in_an_Array.cpp
@@ -28,3 +28,16 @@ public:
         return (nums[k - 1]);
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> queue;
+        for (int i = 0; i < nums.size(); i++) {
+            queue.push(nums[i]);
+            if (queue.size() > k)
+                queue.pop();
+        }
+        return queue.top();
+    }
+};

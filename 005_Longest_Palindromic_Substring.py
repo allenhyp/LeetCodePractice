8a578aa14634f5a2ac886834d39a5d59c78b978f
Allen Hsu <bboypupu@gmail.com>
2018-12-09 15:28:37 -0800

    [Modify] 005.py (dp)

--- 005_Longest_Palindromic_Substring.py
+++ 005_Longest_Palindromic_Substring.py
@@ -17,3 +17,19 @@ class Solution(object):
                 head = i - maxLen
                 maxLen += 1
         return s[head:head + maxLen]
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ''
+        n = len(s)
+        dp = [[False for cols in range(n)] for rows in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                dp[i][j] = s[i] == s[j] and (j - i < 3 or dp[i + 1][j - 1])
+                if dp[i][j] and j - i + 1 > len(res):
+                    res = s[i:j + 1]
+        return res

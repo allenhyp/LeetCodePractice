a976ddadcaa7dd33d888c9845da756e856ae70d3
Allen Hsu <bboypupu@gmail.com>
2019-01-15 22:16:32 -0800

    [Add] 049.py

---
+++ 049_Group_Anagrams.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        d = {}
+        for w in strs:
+            key = tuple(sorted(w))
+            d[key] = d.get(key, []) + [w]
+        return d.values()
+        

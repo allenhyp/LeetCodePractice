c6e1f80351619dbd470d96226ead3da843226baa
Allen Hsu <bboypupu@gmail.com>
2019-01-19 16:06:29 -0800

    [Modify] 253.py (heap)

--- 253_Meeting_Rooms_II.py
+++ 253_Meeting_Rooms_II.py
@@ -25,3 +25,24 @@ class Solution:
             else:
                 cur_earliest_end_int += 1
         return total_req
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        intervals = sorted(intervals, key=lambda x: x.start)
+        allocated = []
+        for interval in intervals:
+            if allocated and allocated[0] <= interval.start:
+                heapq.heapreplace(allocated, interval.end)
+            else:
+                heapq.heappush(allocated, interval.end)
+        return len(allocated)

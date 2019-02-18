f6a4faaf2ba58f8c6afe1f7401421d338d9ce68f
Allen Hsu <bboypupu@gmail.com>
2019-02-18 13:41:01 -0800

    [Add] 264.py

---
+++ 264_Ugly_Number_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nthUglyNumber(self, n: 'int') -> 'int':
+        dp = [1] * n
+        p2 = p3 = p5 = 0
+        for i in range(1, n):
+            dp[i] = min(dp[p2] * 2, dp[p3] * 3, dp[p5] * 5)
+            if dp[i] == dp[p2] * 2:
+                p2 += 1
+            if dp[i] == dp[p3] * 3:
+                p3 += 1
+            if dp[i] == dp[p5] * 5:
+                p5 += 1
+        return dp[-1]

a5cd39e1312828a850c923ee3b8ba872f1907aaa
Allen Hsu <bboypupu@gmail.com>
2019-04-15 17:07:34 -0700

    [Modify] 794.py

--- 794_Valid_Tic-Tac-Toe_State.py
+++ 794_Valid_Tic-Tac-Toe_State.py
@@ -1,4 +1,12 @@
 class Solution:
+    def validTicTacToe(self, board: List[str]) -> bool:
+        b = '|'.join(board)
+        x, o = (any(p * 3 in b[s::d] for s in range(9) for d in [1, 3, 4, 5]) for p in 'XO')
+        m = b.count('X') - b.count('O')
+        return (m == 0 and not x) or (m == 1 and not o)
+
+
+class Solution:
     def check(self, state):
         if state[0] == state[1] and state[1] == state[2]:
             if state[0] == 'X':

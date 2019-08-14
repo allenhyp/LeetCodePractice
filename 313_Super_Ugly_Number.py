8ee4ae801999d23b56a711daf4ef5739857e781a
Allen.Hsu <bboypupu@gmail.com>
2019-08-13 18:57:18 -0700

    [Modify] 313.py (priority queue)

--- 313_Super_Ugly_Number.py
+++ 313_Super_Ugly_Number.py
@@ -10,3 +10,18 @@ class Solution:
             for j in range(k):
                 index[j] += 1 if ugly[i] == ugly[index[j]] * primes[j] else 0
         return ugly[-1]
+
+
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        dp, q = [1], []
+        for prime in primes:
+            heapq.heappush(q, (prime, 0, prime))
+            
+        for _ in range(1, n):
+            min_v, index, prime = q[0]
+            dp.append(min_v)
+            while q[0][0] == min_v:
+                min_v, index, prime = heapq.heappop(q)
+                heapq.heappush(q, (prime * dp[index + 1], index + 1, prime))
+        return dp[-1]

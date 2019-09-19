66fedc37bc20ee75cec148148e5bdd8c41b0c2f0
Allen.Hsu <bboypupu@gmail.com>
2019-09-19 16:15:21 -0700

    [Modify] 347.py

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -1,10 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic, q, res = {}, [], []
-        for n in nums:
-            dic[n] = dic.get(n, 0) + 1
-        for key, val in dic.items():
-            heapq.heappush(q, (-val, key))
-        for i in range(k):
-            res.append(heapq.heappop(q)[1])
-        return res
+        count = collections.Counter(nums)
+        return sorted(count, key=count.get, reverse=True)[:k]

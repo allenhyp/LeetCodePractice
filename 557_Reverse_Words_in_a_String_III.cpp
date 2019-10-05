056df50aba422e1b9ed1adaaed72a8192c5fc6d8
allenhyp <bboypupu@gmail.com>
2019-10-05 00:29:15 -0700

    [Add] 557.cpp

---
+++ 557_Reverse_Words_in_a_String_III.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int front = 0;
+        for (int i = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] == ' ') {
+                reverse(&s[front], &s[i]);
+                front = i + 1;
+            }
+        }
+        return s;
+    }
+};

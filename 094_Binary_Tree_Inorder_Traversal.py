433bbab6f6f896de7bc88d333b41ddc0203aebe2
Allen.Hsu <bboypupu@gmail.com>
2019-11-06 14:24:04 -0800

    [Modify] 094.py (Morris traversal with O(1) space)

--- 094_Binary_Tree_Inorder_Traversal.py
+++ 094_Binary_Tree_Inorder_Traversal.py
@@ -19,3 +19,32 @@ class Solution:
                 stack.pop()
                 t = p.right
         return res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        cur = root
+        while cur:
+            if cur.left is None:
+                res.append(cur.val)
+                cur = cur.right
+            else:
+                pre = cur.left
+                while pre.right is not None and pre.right != cur:
+                    pre = pre.right
+                if pre.right is None:
+                    pre.right = cur
+                    cur = cur.left
+                else:
+                    pre.right = None
+                    res.append(cur.val)
+                    cur = cur.right
+        return res

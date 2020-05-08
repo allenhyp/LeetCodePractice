41a743224379271b9abae329620ca1b26ab4eecb
Allen.Hsu <bboypupu@gmail.com>
2020-05-07 17:29:14 -0700

    [Modify] 993.py (Add BFS solution)

--- 993_Cousins_in_Binary_Tree.py
+++ 993_Cousins_in_Binary_Tree.py
@@ -27,3 +27,21 @@ class Solution:
             else:
                 return max(traversal(root.right, depth + 1, root.val), traversal(root.left, depth + 1, root.val))
         return traversal(root, 0, root.val) == 1
+
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        level = [(root, None)]
+        while len(level):
+            next_level, found = [], []
+            for node in level:
+                if node[0].val in [x, y]:
+                    found.append(node)
+                if node[0].left is not None: next_level.append((node[0].left, node))
+                if node[0].right is not None: next_level.append((node[0].right, node))
+            if len(found) > 0:
+                if len(found) == 2 and found[0][1] != found[1][1]:
+                    return True
+                return False
+            level = next_level
+        return False

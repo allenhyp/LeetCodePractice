7315f1f434e42431745fed2799ddca77cb8508fd
bboypupu <bboypupu@gmail.com>
2020-06-07 22:05:36 -0700

    [Add] 406.cpp

---
+++ 406_Queue_Reconstruction_by_Height.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    static bool compare(vector<int> a, vector<int> b) {
+        if (a[0] == b[0]) return a[1] < b[1];
+        else return a[0] > b[0];
+    }
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), compare);
+        vector<vector<int>> res;
+        for (auto p : people) {
+            res.insert(res.begin() + p[1], p);
+        }
+        return res;
+    }
+};

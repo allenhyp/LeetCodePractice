74687bd4cadfc9bb18d9547abb14b2b6823ab2a1
Allen.Hsu <bboypupu@gmail.com>
2020-06-10 13:35:47 -0700

    [Modify] 035.py

--- 035_Search_Insert_Position.py
+++ 035_Search_Insert_Position.py
@@ -9,3 +9,16 @@ class Solution(object):
             if nums[i] >= target:
                 return i
         return len(nums)
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left

4c407e9537a6252a9e0aa8489d4670280d6fe1ec
Allen.Hsu <bboypupu@gmail.com>
2020-06-23 19:11:02 -0700

    [Modify] 222.py

--- 222_Count_Complete_Tree_Nodes.py
+++ 222_Count_Complete_Tree_Nodes.py
@@ -10,3 +10,21 @@ class Solution:
         if root is None:
             return 0
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        def depth(root):
+            if not root: return 0
+            return 1 + depth(root.left)
+        if not root: return 0
+        left, right = depth(root.left), depth(root.right)
+        if left == right:
+            return 2 ** left + self.countNodes(root.right)
+        else:
+            return 2 ** right + self.countNodes(root.left)

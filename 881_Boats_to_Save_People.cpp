66366938f9ca6818303765e13787b49fd74f9d3f
Allen.Hsu <bboypupu@gmail.com>
2021-01-13 19:32:59 -0800

    [Add] 881.cpp

---
+++ 881_Boats_to_Save_People.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int i = 0, j = people.size() - 1;
+        sort(people.rbegin(), people.rend());
+        for (; i <= j; ++i) {
+            if (people[i] + people[j] <= limit) --j;
+        }
+        return i;
+    }
+};

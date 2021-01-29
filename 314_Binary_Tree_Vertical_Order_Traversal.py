b7fd61befa499d1162537b37ddf9e95337f59d9c
Allen.Hsu <bboypupu@gmail.com>
2021-01-29 12:36:34 -0800

    [Modify] 314.py

--- 314_Binary_Tree_Vertical_Order_Traversal.py
+++ 314_Binary_Tree_Vertical_Order_Traversal.py
@@ -50,3 +50,22 @@ class Solution:
                 cur = [val]
         res.append(cur)
         return res
+
+
+class Solution:
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        res, level, left, right = [[]], [(root, 0)], 0, 0
+        while level:
+            nxtl = []
+            for node, idx in sorted(level, key=lambda x: x[0].val):
+                if idx < left:
+                    res.insert(0, [])
+                    left -= 1
+                elif idx > right:
+                    res.append([])
+                    right += 1
+                res[idx - left].append(node.val)
+                if node.left: nxtl.append((node.left, idx - 1))
+                if node.right: nxtl.append((node.right, idx + 1))
+            level = nxtl
+        return res

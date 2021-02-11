d41f9e91f029a87e759add3964f04528589b8051
Allen.Hsu <bboypupu@gmail.com>
2021-02-11 14:51:16 -0800

    [Add] 242.cpp

---
+++ 242_Valid_Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int cnt[26] = {0};
+        for (char c : s) ++cnt[c - 'a'];
+        for (char c : t) --cnt[c - 'a'];
+        for (int i = 0; i < 26; ++i){
+            if (cnt[i] != 0) return false;
+        }
+        return true;
+    }
+};

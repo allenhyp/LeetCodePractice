c1c9055125d9ab561a798424238e215a6ad5a7bb
bboypupu <bboypupu@gmail.com>
2021-02-20 18:38:03 -0800

    [Modify] 13.py

--- 013_Roman_To_Integer.py
+++ 013_Roman_To_Integer.py
@@ -25,28 +25,14 @@ class Solution1(object):
         return result
 
 
-class Solution2(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        lookUpTable = {'I': 1,
-                       'V': 5,
-                       'X': 10,
-                       'L': 50,
-                       'C': 100,
-                       'D': 500,
-                       'M': 1000
-                       }
-        result = 0
-        thisInput = 0
-        for c in s[::1]:
-            nextInput = lookUpTable[c]
-            if thisInput < nextInput:
-                result -= thisInput
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = level = 0
+        for c in s[::-1]:
+            if dic[c] >= level:
+                res += dic[c]
+                level = dic[c]
             else:
-                result += thisInput
-            thisInput = nextInput
-        result += nextInput
-        return result
+                res -= dic[c]
+        return res

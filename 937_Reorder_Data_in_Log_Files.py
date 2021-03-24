a9ddbdf871c048864aad1872b9e1ded2ff007e79
bboypupu <bboypupu@gmail.com>
2021-03-23 21:23:39 -0700

    [Modify] 937.py

--- 937_Reorder_Data_in_Log_Files.py
+++ 937_Reorder_Data_in_Log_Files.py
@@ -8,3 +8,14 @@ class Solution:
                 letters.append(log)
         letters = sorted(letters, key=lambda x: str(x.split()[1:]) + x.split()[0])
         return letters + digits
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def transferLog2Key(log):
+            left, right = log.split(' ', 1)
+            if right[0].isdigit():
+                return (1, )
+            else:
+                return (0, right, left)
+        return sorted(logs, key=transferLog2Key)

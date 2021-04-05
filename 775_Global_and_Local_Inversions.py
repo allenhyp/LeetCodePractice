ca72188b0da3c9f4b6ad223227591a6d77ff6d0b
bboypupu <bboypupu@gmail.com>
2021-04-05 14:19:23 -0700

    [Add] 775.py

---
+++ 775_Global_and_Local_Inversions.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isIdealPermutation(self, A: List[int]) -> bool:
+        for i, a in enumerate(A):
+            if abs(a - i) > 1:
+                return False
+        return True

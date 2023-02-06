a288cdf9125790a3c7d3e38d85788c4769664b1d
Yuan-Pu (Allen) Hsu <bboypupu@gmail.com>
2023-02-06 09:27:58 +0000

    [Add] 1470.py

---
+++ 1470_Shuffle_the_Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ret = []
+        for i in range(n):
+            ret.extend([nums[i], nums[n + i]])
+        return ret

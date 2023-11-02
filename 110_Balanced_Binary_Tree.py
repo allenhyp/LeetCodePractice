a6bbc1aedf9ab8dda3807ddba1b724388c85d093
Allen Hsu <allen.yph@gmail.com>
2023-11-02 01:39:09 -0700

    [Modify] 110.py

--- 110_Balanced_Binary_Tree.py
+++ 110_Balanced_Binary_Tree.py
@@ -1,5 +1,23 @@
 # Definition for a binary tree node.
 # class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def traverse(node):
+            if not node:
+                return 0, True
+            leftHeight, leftBalanced = traverse(node.left)
+            rightHeight, rightBalanced = traverse(node.right)
+            return max(leftHeight, rightHeight) + 1, abs(leftHeight - rightHeight) <= 1 and leftBalanced and rightBalanced
+        _, b = traverse(root)
+        return b
+
+
+# Definition for a binary tree node.
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None

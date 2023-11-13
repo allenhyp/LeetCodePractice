fd25a924dc101be140a08e4cf5caba43f9b08f5b
Allen Hsu <allen.yph@gmail.com>
2023-11-12 19:19:34 -0800

    [Modify] 238.py

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -1,4 +1,17 @@
 class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        prefix, suffix = [1] * (size + 1) , [1] * (size + 1)
+        for i, n in enumerate(nums):
+            prefix[i + 1] = prefix[i] * n
+        for i, n in reversed(list(enumerate(nums))):
+            suffix[i] = suffix[i + 1] * n
+        ret = [0] * size
+        for i in range(size):
+            ret[i] = prefix[i] * suffix[i + 1]
+        return ret
+
+class Solution:
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]

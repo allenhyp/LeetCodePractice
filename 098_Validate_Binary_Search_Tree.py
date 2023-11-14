fff8591f661c93c3c8e988b62c41d292fdcf1ff3
Allen Hsu <allen.yph@gmail.com>
2023-11-13 16:36:54 -0800

    [Modify] 98.py

--- 098_Validate_Binary_Search_Tree.py
+++ 098_Validate_Binary_Search_Tree.py
@@ -14,3 +14,26 @@ class Solution:
                 return False
             return helper(root.left, min_node, root) and helper(root.right, root, max_node)
         return helper(root, None, None)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def inOrder(node, arr):
+            if not node:
+                return arr
+            arr = inOrder(node.left, arr)
+            arr.append(node.val)
+            arr = inOrder(node.right, arr)
+            return arr
+        
+        arr = inOrder(root, [])
+        for i in range(1, len(arr)):
+            if arr[i] <= arr[i - 1]:
+                return False
+        return True        

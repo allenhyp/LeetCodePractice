6488aeecfe2316b2cf5a292c6e4937f6574641d0
Yuan-Pu Hsu <allen.yph@gmail.com>
2023-11-20 00:14:10 -0800

    [Modify] 981.py

--- 981_Time_Based_Key-Value_Store.py
+++ 981_Time_Based_Key-Value_Store.py
@@ -1,24 +1,26 @@
+from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.queue = {}
+        self.map = defaultdict(list)
 
-    def set(self, key: 'str', value: 'str', timestamp: 'int') -> 'None':
-        if key in self.queue:
-            heapq.heappush(self.queue[key], (-timestamp, value))
-        else:
-            self.queue[key] = [(-timestamp, value)]
-
-    def get(self, key: 'str', timestamp: 'int') -> 'str':
-        if key in self.queue:
-            for t, v in self.queue[key]:
-                if -t <= timestamp:
-                    return v
-        return ''
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append((value, timestamp))
         
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map or timestamp < self.map[key][0][1]:
+            return ""
+        arr = self.map[key]
+        left, right = 0, len(arr)
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid][1] == timestamp:
+                return arr[mid][0]
+            if arr[mid][1] > timestamp:
+                right = mid
+            else:
+                left = mid + 1
+        return arr[left - 1][0]
 
 
 # Your TimeMap object will be instantiated and called as such:

749057660c9ea4597f6ccf9e4fecbc79785f77aa
Yuan-Pu Hsu <allen.hsu@Yuan-Pus-MacBook-Air.local>
2023-11-19 19:46:46 -0800

    [Modify] 236.py

--- 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -18,3 +18,23 @@ class Solution:
                        for child in (root.left, root.right))
         return root if left and right else left or right
     
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root in (None, p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if not left:
+            return right
+        elif not right:
+            return left
+        else:
+            return root

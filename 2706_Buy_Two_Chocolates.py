27f5c31c3bbee39f95f865054d29f6aeb143240b
Yuan-Pu Hsu <allen.yph@gmail.com>
2023-12-20 16:46:08 +0800

    [Add] 2706.py

---
+++ 2706_Buy_Two_Chocolates.py
@@ -0,0 +1,5 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        ret = money - prices[0] - prices[1]
+        return ret if ret >= 0 else money

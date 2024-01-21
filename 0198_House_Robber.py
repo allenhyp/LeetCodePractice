c5a564d4be711e6f6d9a13513d3123850bb265ff
Yuan-Pu Hsu <allen.yph@gmail.com>
2024-01-21 12:43:15 -0800

    [Modify] 0198.py, 0198.cpp

--- 0198_House_Robber.py
+++ 0198_House_Robber.py
@@ -25,12 +25,10 @@ class Solution:
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 0: return 0
-        memo = [0] * (n + 1)
-        memo[1] = nums[0]
+        dp = [0, nums[0]]
         for i in range(1, n):
-            memo[i + 1] = max(memo[i], memo[i - 1] + nums[i])
-        return memo[-1]
+            dp.append(max(dp[i - 1] + nums[i], dp[i]))
+        return dp[-1]
 
 
 # Iteration with O(1) space

937c2239a50d545136bc6a4c71096b8f2251eaf8
Yuan-Pu Hsu <allen.yph@gmail.com>
2024-03-17 09:42:37 -0600

    [Add] 0057.cpp [Modify] 0057.py

---
+++ 0057_Insert_Interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        bool inserted = false;
+        for (auto interval : intervals) {
+            if (interval[1] < newInterval[0]) res.push_back(interval);
+            else if (interval[0] > newInterval[1]) {
+                if (!inserted) {
+                    res.push_back(newInterval);
+                    inserted = true;
+                }
+                res.push_back(interval);
+            } else {
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
+            }
+        }
+        if (!inserted) res.push_back(newInterval);
+        return res;
+    }
+};

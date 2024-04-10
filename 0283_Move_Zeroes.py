029968bf5f760916748579e238b90da418240f69
Yuan-Pu Hsu <allen.yph@gmail.com>
2024-04-10 13:08:02 -0700

    [Modify] 0283.py

--- 0283_Move_Zeroes.py
+++ 0283_Move_Zeroes.py
@@ -15,3 +15,16 @@ class Solution:
         
         for j in range(i,len(nums)):
             nums[j] = 0
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        j = 0
+        for i in range(n):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        return

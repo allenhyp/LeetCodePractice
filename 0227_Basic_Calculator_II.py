0b3b6b6198bc3e2311fbb5c86fc0562157633d0a
Yuan-Pu Hsu <allen.yph@gmail.com>
2024-05-01 13:19:12 -0700

    [Modify] 0227.py, 0227.cpp

--- 0227_Basic_Calculator_II.py
+++ 0227_Basic_Calculator_II.py
@@ -1,19 +1,22 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack, num, sign = [], 0, '+'
-        for i in range(len(s)):
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            if not s[i].isdigit() and not s[i].isspace() or i == len(s) - 1:
-                if sign == '+':
-                    stack.append(num)
-                elif sign == '-':
-                    stack.append(-num)
-                elif sign == '*':
-                    stack.append(stack.pop() * num)
-                elif sign == '/':
-                    stack.append(int(stack.pop() / num))
-                sign, num = s[i], 0
+        stack = []
+        sign, cur = '+', 0
+        for c in s + '+':
+            if c.isdigit():
+                cur = cur * 10 + int(c)
+            elif c != ' ':
+                match sign:
+                    case '+':
+                        stack.append(cur)
+                    case '-':
+                        stack.append(-cur)
+                    case '*':
+                        stack.append(stack.pop()*cur)
+                    case '/':
+                        stack.append(int(stack.pop()/cur))
+                sign, cur = c, 0
+
         return sum(stack)
 
 
